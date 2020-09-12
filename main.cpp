f7b25eea946a91b794d06e345bfb0450c06b96eb
Ldragon27 <lomondragon9127@gmail.com>
2020-09-11 22:20:45 -0600

    Final

--- main.cpp
+++ main.cpp
@@ -8,6 +8,18 @@ using namespace std;
 Point::Point(double x, double y) : xValue(x), yValue(y) {}
 
 //*********************************************
+//getX returns x coordinate
+//*********************************************
+double Point::getX() const{
+    return xValue;
+}
+//*********************************************
+//getY returns y coordinate
+//*********************************************
+double Point::getY() const{
+    return yValue;
+}
+//*********************************************
 //Rectangle Constructor
 //*********************************************
 Rectangle::Rectangle(int l, int w, double x, double y) : length(l), width(w), topLeft(Point(x, y)) {}
@@ -28,16 +40,27 @@ int Rectangle::getWidth() const
 }
 
 //*********************************************
-//width prints statment
 //
-// x = 0
-// y = 1
+//*********************************************
+void intersect(Rectangle r1, Rectangle r2) {
+    if (r1.topLeft.getX() < r2.topLeft.getX() + r2.getWidth() &&
+        r1.topLeft.getX() + r1.getWidth() > r2.topLeft.getX() &&
+        r1.topLeft.getY() < r2.topLeft.getY() + r2.getLength() &&
+        r1.topLeft.getY() + r1.getLength() > r2.topLeft.getY())
+        cout << "Collision Detected\n";
+    else {
+        cout << "No Collision Detected\n";
+    }
+    return;
+}
+//*********************************************
+//
 //*********************************************
 Rectangle makeRect(const char *id)
 {
     int l, w; // a is length & b is width
     double x, y; // x is x-coordinate & y is y-coordinate
-    cout << "Enter width of rectangle " << id << "\n";
+    cout << "Enter width of rectangle: " << id << "\n";
     cin >> w;
     cout << "Enter length of the rectangle: " << id << "\n";
     cin >> l;
@@ -54,5 +77,6 @@ int main()
     cout << "This program will take inputs for two rectangles and inform if they intersect" << endl;
     Rectangle r1 = makeRect("1");// instance of rectangle class
     Rectangle r2 = makeRect("2");
+    intersect(r1,r2);
     return 0;
 }
--- main.h
+++ main.h
@@ -5,10 +5,12 @@
 class Point {
 public:
     Point(double x, double y);
-
+    double getX() const;
+    double getY() const;
 private:
     double xValue;
     double yValue;
+
 };
 
 
@@ -17,10 +19,12 @@ public:
     Rectangle(int length, int width, double x, double y);
     int getLength() const;
     int getWidth() const;
+    Point topLeft;
+    void intersect(Rectangle r1, Rectangle r2) const;
+
 private:
     int length;
     int width;
-    Point topLeft;
 };
 
 #endif
